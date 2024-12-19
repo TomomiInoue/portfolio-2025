7a97adfd8550af7963f4d84ae16f340b7ee242d7
Tomomi Inoue <tomomi@helloagain.com.au>
2024-12-19 16:46:37 +0700

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,44 @@
-import type { Config } from "tailwindcss";
+const extendedTheme = {
+  colors: {
+    primary: "#fff8ed",
+    secondary: "#393636",
+    accent: "#2273f3",
+  },
+  fontSize: {
+    display01: ["48px", { lineHeight: "120%", fontWeight: "600", fontFamily: "Abril Fatface" }],
+    display02: ["60px", { lineHeight: "120%", fontWeight: "600", fontFamily: "Abril Fatface" }],
+  }
+}
 
-export default {
+const tailwindConfig = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        // next-ui
+    "../../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    extend: extendedTheme,
+    container: {
+        center: true,
+        screens: {
+          xs: "100%",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1152px",
+          "2xl": "1152px",
+        },
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "3rem",
+          lg: "0rem",
+          xl: "0rem",
+        },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default tailwindConfig;
